050ee40573651ed07f5a4a731222c59045a78366
weitero <112836181+weitero@users.noreply.github.com>
2023-02-22 21:34:38 +0800

    returns a list

--- bioinformatics_stronghold/dna-counting_dna_nucleotides.py
+++ bioinformatics_stronghold/dna-counting_dna_nucleotides.py
@@ -3,6 +3,6 @@ def solve_it(s: str):
     Returns four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occurs in `s`.
 
     >>> solve_it('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC')
-    '20 12 17 21'
+    [20, 12, 17, 21]
     """
-    return ' '.join([str(s.count(x)) for x in ['A', 'C', 'G', 'T']])
+    return [s.count('A'), s.count('C'), s.count('G'), s.count('T')]

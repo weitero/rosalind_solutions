15d79e5ec12868a0b96faa4785987cebd8139147
weitero <112836181+weitero@users.noreply.github.com>
2023-02-27 23:06:07 +0800

    update

--- bioinformatics_armory/ini-introduction_to_the_bioinformatics_armory.py
+++ bioinformatics_armory/ini-introduction_to_the_bioinformatics_armory.py
@@ -1,1 +1,8 @@
-../bioinformatics_stronghold/dna-counting_dna_nucleotides.py
+def solve_it(s: str) -> list[int]:
+    """
+    Returns four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occurs in `s`. Note: You must provide your answer in the format shown in the sample output below.
+
+    >>> solve_it('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC')
+    [20, 12, 17, 21]
+    """
+    return [s.count(b) for b in 'ACGT']
